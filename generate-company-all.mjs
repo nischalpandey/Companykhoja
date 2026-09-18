0cafab4d57bcc77ddc2ecf08013aa6df2f25fee4
Nischal Pandey <admin@nischalpandey.com.np>
2026-09-18 11:01:03 +0545

    Generate company SEO pages

--- generate-company-all.mjs
+++ generate-company-all.mjs
@@ -9,7 +9,7 @@ import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
-const ROOT = join(__dirname, '..')
+const ROOT = __dirname
 
 const DATA_FILE = join(
   ROOT,
